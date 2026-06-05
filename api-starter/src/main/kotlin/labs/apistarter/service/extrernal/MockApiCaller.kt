8e1b272ce9c2b9ec1b4e99545c28eddafa13e50e
Kyrylo Kusovskyi <kkirilll2006@gmail.com>
2026-06-05 08:34:57 +0300

    Small fix

--- api-starter/src/main/kotlin/labs/apistarter/service/extrernal/MockApiCaller.kt
+++ api-starter/src/main/kotlin/labs/apistarter/service/extrernal/MockApiCaller.kt
@@ -1,6 +1,8 @@
 package labs.apistarter.service.extrernal
 
-@Ser
+import org.springframework.stereotype.Service
+
+@Service
 class MockApiCaller : ExternalApiCaller {
     override fun callStart() {
         println("Request has sent to start watching all-clear")
